7e2a07c8e5c121a1e31f1b47f4cb531029e2f088
evantimms <timmsevan@gmail.com>
2019-01-28 13:40:53 -0700

    added basic heat chart example

--- src/App.js
+++ src/App.js
@@ -1,57 +1,61 @@
 import React from 'react';
 // import HeatTable from './heat-table'
 import HeatTable from './heat-table'
+import * as d3 from 'd3'
 
-const data = {
-  xGroupBy: 'Well Cost',
-  yGroupBy: 'Well Production (3mo)',
-  colorBy: 'Cost',
-  xData: [],
-  yData: [],
-  frequency: []
+const width = 700
+const height = 500
+
+const margin = {
+  left: 85,
+  top: 30,
+  right: 20,
+  bottom: 45
 }
+
+const thresholds = [
+  {
+    'value': 90,
+    'T': 95,
+    'H': 31
+  },
+  {
+    'value': 103,
+    'T': 102,
+    'H': 38
+  },
+  {
+    'value': 125,
+    'T': 107,
+    'H': 50
+  },
+  {
+    'value': 165,
+    'T': 113,
+    'H': 72
+  }
+]
+
+
+const xAxisTicks = d3.range(10,101,1)
+
+const yAxisTicks = d3.range(80,121,1).reverse()
 
 class App extends React.Component {
 
-  _getData () {
-    while(data.xData.length < 100){
-      data.xData.push(Math.floor(Math.random()*100))
-      data.yData.push(Math.floor(Math.random()*100))
-      data.frequency.push(Math.floor(Math.random() * 5))
-    }
-  }
 
   render() {
-    const margin = { top: 50, right: 0, bottom: 100, left: 30 }
-    const width = 960 - margin.left - margin.right
-    const height = 640 - margin.top - margin.bottom
-
-    this._getData()
     return (
       <div 
         className="App"
       >
-        {/* <HeatTable
-          id='prism-heat-table'
-          dataFile='data.tsv'
+        <HeatTable
           margin={margin}
           width={width}
           height={height}
-          buckets={9}
-          colors={["#ffffd9","#edf8b1","#c7e9b4","#7fcdbb","#41b6c4","#1d91c0","#225ea8","#253494","#081d58"]}
-          xLabelNames={["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"]}
-          yLabelNames={["1a", "2a", "3a", "4a", "5a", "6a", "7a", "8a", "9a", "10a", "11a", "12a", "1p", "2p", "3p", "4p", "5p", "6p", "7p", "8p", "9p", "10p", "11p", "12p"]}
-          gridSize = {Math.floor(width / 24)}
-          legendElementWidth = {gridSize*2}
-        /> */}
-        <HeatTable 
-          margin={margin}
-          width={width}
-          height={height}
-          colors={["#ffffd9","#edf8b1","#c7e9b4","#7fcdbb","#41b6c4","#1d91c0","#225ea8","#253494","#081d58"]}
-          data={data}
-          bins={10}
-          gridSize={Math.floor(width / 24)}
+          yAxisTicks={yAxisTicks}
+          xAxisTicks={xAxisTicks}
+          thresholds={thresholds}
         />
       </div>
     );
--- src/heat-table.js
+++ src/heat-table.js
@@ -17,69 +17,163 @@ export default class HeatTable extends React.Component {
         this._renderHeatTable()
     }
 
-    _getLabels (lower,upper, divisions) {
-        let toReturn = []
-        let mean = Math.ceil((lower+upper)/divisions)
-        for(let i = 0; i < divisions + 1; ++i){
-            toReturn.push(lower + i * mean)
-        }
-        return toReturn
-    }
 
     _renderHeatTable () {
-        const { margin, width, height, data, colors, bins, gridSize } = this.state
+        const { width, height, margin, xAxisTicks, yAxisTicks, thresholds } = this.state 
 
-        const xLabel = this.state.data.xGroupBy
-        const yLabel = this.state.data.yGroupBy
-        const xMax = Math.max.apply(null,this.state.data.xData)
-        const xMin = Math.min.apply(null,this.state.data.xData)
-        const yMax = Math.max.apply(null,this.state.data.yData)
-        const yMin = Math.min.apply(null,this.state.data.yData)
-        const colorByMin = Math.max.apply(null,this.state.data.frequency)
-        const colorByMax = Math.min.apply(null,this.state.data.frequency)
 
-        // Create svg
-        const svg = d3.select('.heat-table')
-                        .append('svg')
-                        .attr('width', width + margin.left + margin.right)
-                        .attr('height', height + margin.top + margin.bottom)
-                        .append('g')
-                        .attr('transform', `translate(${margin.left},${margin.top})`)
+        const f = (a, b) => [].concat(...a.map(a => b.map(b => [].concat(a, b))))
+        const cartesian = (a, b, ...c) => b ? cartesian(f(a, b), ...c) : a
 
-        const chart = svg.append('g')
-                            .attr('transform', `translate(${margin}, ${margin})`)
+        const HI = (T, RH) => {
+            return -42.379 + 2.04901523*T + 10.14333127*RH - .22475541*T*RH - .00683783*T*T - .05481717*RH*RH + .00122874*T*T*RH + .00085282*T*RH*RH - .00000199*T*T*RH*RH;
+        }
+
+
+        const thresholdScale = d3.scaleThreshold()
+        .domain(thresholds.map(d => d.value))
+        .range([0,1,2,3,4])
+
+        const values = cartesian(yAxisTicks, xAxisTicks).map(d => {
+            return {
+              temp: d[0],
+              rh: d[1],
+              HI: HI(...d),
+              threshold: thresholdScale(HI(...d))
+            }
+          })
         
-        const xLabels = this._getLabels(xMin,xMax,bins)
-        const yLabels = this._getLabels(yMin,yMax,bins)
+        const chartData = d3.nest()
+        .key(d => d.threshold)
+        .sortKeys(d3.ascending)
+        .key(d => d.rh)
+        .rollup(vals => d3.max(vals))
+        .entries(values)
+        .map(d => {
+            return {
+            key: d.key,
+            values: xAxisTicks.map(h => {
+                const v = d.values.find(f => +f.key == h);
+                return {
+                rh: h,
+                temp: v ? v.value.temp : 120
+                }
+            })
+            }
+        }).map( (d, i, arr) => {
+            d.values = d.values.map( (f, j) => {
+            f.prevTemp = i > 0 ? arr[i - 1].values[j].temp : 80;
+            return f;
+            })
+            return d;
+        })
 
-        const yScale = d3.scaleBand()
-                        .range([height,0])
-                        .domain(yLabels)
-                        
-        chart.append('g')
-                .call(d3.axisLeft(yScale))
 
-        const xScale = d3.scaleBand()
-                        .range([0,width])
-                        .domain(xLabels)
-                        .padding(0.2)
-
-        chart.append('g')
-                .attr('transform', `translate(0, ${height})`)
-                .call(d3.axisBottom(xScale))
+        const colors = ['#fdf7e1','#faeaae','#f7d790','#f3b473','#fd8d3c','#fc4e2a','#e31a1c','#b10026']
         
-        chart.append('g')
-            .attr('transform', `translate(0, ${height})`)
+        
 
-        // colorScale
-        const colorScale = d3.scaleQuantile()
-                            .domain([colorByMin, colorByMax])
-                            .range(colors)
-
-        //Cards 
-        const cards = svg.selectAll('.card')
-                            .data(data, (d) => d.xDat)        
-
+        const svg = d3.select('.heat-table').append('svg')
+                            .attr('width', width)
+                            .attr('height', height)
+          
+          const chartWidth = width - margin.left - margin.right;
+          const chartHeight = height - margin.top - margin.bottom;
+        
+          const chart = svg.append('g')
+          .style('font-family', 'sans-serif')
+          .attr('transform', `translate(${margin.left}, ${margin.top})`);
+        
+          const x = d3.scaleLinear()
+          .domain(d3.extent(xAxisTicks))
+          .range([0, chartWidth]);
+        
+          const y = d3.scaleLinear()
+          .domain(d3.extent(yAxisTicks))
+          .range([chartHeight, 0]);
+        
+          chart.append("g")
+          .attr('transform', `translate(0, ${chartHeight + 2})`)
+          .style('font-size', '13px')
+          .style('font-weight', 300)
+          .call(d3.axisBottom(x).ticks(5))
+          .call(g => g.select('.tick:last-of-type text').text('100%'))
+          .selectAll('.domain').remove();
+        
+          chart.append('text')
+          .attr('x', chartWidth)
+          .attr('y', chartHeight + 40)
+          .text('Relative humidity')
+          .style('text-anchor', 'end')
+          .style('font-weight', 600)
+          .style('font-size', '14px');
+        
+          chart.append("g")
+          .attr('transform', 'translate(-2, 0)')
+          .style('font-size', '13px')
+          .style('font-weight', 300)
+          .call(d3.axisLeft(y).ticks(5))
+          .call(g => g.select('.tick:last-of-type text').text('120 °F'))
+          .selectAll('.domain').remove();
+        
+          chart.append('text')
+          .attr('x', 0)
+          .attr('y', -15)
+          .text('Temperature')
+          .style('text-anchor', 'end')
+          .style('font-weight', 600)
+          .style('font-size', '14px');
+          
+          const area = d3.area()
+          .x(d => x(d.rh))
+          .y0(d => y(d.temp))
+          .y1(d => y(d.prevTemp))
+          .curve(d3.curveStepAfter);
+          
+          const paths = chart.append('g')
+          .selectAll('path')
+          .data(chartData)
+          .enter().append("path")
+          .attr('d', d => area(d.values))
+          .style('fill', d => colors[+d.key])
+          .style('opacity', .8);
+          
+          chart.append('g')			
+          .attr('transform', `translate(0, ${chartHeight})`)
+          .call(d3.axisBottom(x)
+            .ticks(xAxisTicks.length)
+            .tickSize(-chartHeight)
+            .tickFormat('')
+          ).selectAll('line')
+          .style('stroke', 'white')
+          .style('stroke-width', 1)
+          .style('stroke-opacity', .5);
+          
+          chart.append('g')			
+          .call(d3.axisLeft(y)
+            .ticks(yAxisTicks.length)
+            .tickSize(-chartWidth)
+            .tickFormat('')
+          ).selectAll('line')
+          .style('stroke', 'white')
+          .style('stroke-width', 1)
+          .style('stroke-opacity', .5);
+          
+          chart.selectAll('.domain').remove();
+          
+          chart.append('g')
+          .selectAll('text')
+          .data(thresholds)
+          .enter().append('text')
+          .text(d => d.text)
+          .attr('transform', d => `translate(${x(d.H)}, ${y(d.T) - 2})`)
+          .attr('x', 0)
+          .attr('y', 0)
+          .style('text-anchor', 'middle')
+          .style('font-size', '13px')
+          .style('font-weight', 300);
+          
+          return svg.node();
 
     }
 
