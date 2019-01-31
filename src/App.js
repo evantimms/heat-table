862a92d49c886d96c167f78e8acab07cbfacac75
evantimms <timmsevan@gmail.com>
2019-01-31 13:29:57 -0700

    heatmap done, finished refactor and add resizability

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 // import HeatTable from './heat-table'
 import HeatTable from './heat-table'
 
-const width = 700
-const height = 500
+const width = 800
+const height = 800
 
 const margin = {
   left: 85,
@@ -11,35 +11,14 @@ const margin = {
   right: 20,
   bottom: 45
 }
-const thresholds = [
-  {
-    'value': 90,
-    'T': 95,
-    'H': 31
-  },
-  {
-    'value': 103,
-    'T': 102,
-    'H': 38
-  },
-  {
-    'value': 125,
-    'T': 107,
-    'H': 50
-  },
-  {
-    'value': 165,
-    'T': 113,
-    'H': 72
-  }
-]
+
 const colors = ['#fdf7e1','#faeaae','#f7d790','#f3b473','#fd8d3c','#fc4e2a','#e31a1c','#b10026']
 const ranges = {
   xMin: 10,
   yMin: 80,
   xMax: 101,
   yMax: 121,
-  valueMin: 90,
+  valueMin: 0,
   valueMax: 165,
 }
 
@@ -64,23 +43,10 @@ const colorThreshold = [
 
 const xAxisLabel = 'X Axis Label'
 const yAxisLabel = 'Y Axis Label'
-const bins  = 20
-
-function pretendBackEndRequest(ranges, points) {
-  const obj = []
-  
-  for(let i = 0; i < points; ++i){
-    obj.push({
-      x: Math.floor(Math.random() * (ranges.xMax - ranges.xMin) + ranges.xMin),
-      y: Math.floor(Math.random() * (ranges.xMax - ranges.xMin) + ranges.xMin),
-      value: Math.floor(Math.random() * (ranges.valueMax - ranges.valueMin) + ranges.valueMin)
-    })
-  }
-  return obj
+const bins  = {
+  xBins: 30,
+  yBins: 35
 }
-
-
-const data = pretendBackEndRequest(ranges, 100)
 
 class App extends React.Component {
 
@@ -96,12 +62,10 @@ class App extends React.Component {
           xAxisLabel={xAxisLabel}
           yAxisLabel={yAxisLabel}
           height={height}
-          thresholds={thresholds}
           colorThreshold={colorThreshold}
           colors={colors}
           ranges={ranges}
           bins={bins}
-          data={data}
         />
       </div>
     );
--- src/heat-table.js
+++ src/heat-table.js
@@ -17,97 +17,76 @@ export default class HeatTable extends React.Component {
         this._renderHeatTable()
     }
 
-    _calculateXYBinMapping () {
-
-    }
-
-    _mapDToV (x, y) {
-        const { data } = this.state
-
-        data.forEach(el => {
-            if(el.x === x && el.y === y){
-                return el.value
-            }
-        })
-        return 0    
-    }
-
-    _formatData (xAxisTicks, yAxisTicks, colorThreshold) {
-
-        const f = (a, b) => [].concat(...a.map(a => b.map(b => [].concat(a, b))))
-        const cartesian = (a, b, ...c) => b ? cartesian(f(a, b), ...c) : a
-
-        const HI = (T, RH) => {
-            return -42.379 + 2.04901523*T + 10.14333127*RH - .22475541*T*RH - .00683783*T*T - .05481717*RH*RH + .00122874*T*T*RH + .00085282*T*RH*RH - .00000199*T*T*RH*RH;
+    pretendBackEndRequest(ranges, bins) {
+      const data = []
+    
+      for(let i = 0; i < bins.xBins; ++i){
+        const newPoint = []
+        for(let j = 0; j < bins.yBins; ++j){
+          //y
+          newPoint.push(
+            Math.floor(Math.random() * (ranges.valueMax - ranges.valueMin + 1)) + ranges.valueMin
+          )
         }
+        data.push(newPoint)
+      }  
+      return data
+    }
 
 
+    _appendData (data, i, j){
+      if (data[i][j]) return data[i][j]
+    }
+
+    _createBinsFromData (data, xBins, yBins, colorThreshold) {
+      
+
+      const toReturn = []
+      for(let i = 0; i < xBins; ++i){
+        for(let j = 0; j < yBins; ++j){
+          toReturn.push({
+            x: i,
+            y: j,
+            z: this._appendData(data, i, j)
+          })
+        }
+      }
+      return toReturn
+    }
+
+
+    _formatData (data, colorThreshold, bins) {
+        const { xBins, yBins } = bins
+
+        // calculate the threshold based on the value of the z axis
+        // map everything, but only add the threshold if the data point is contained within that binning
+        let values = this._createBinsFromData(data, xBins, yBins, colorThreshold)
         const thresholdScale = d3.scaleThreshold()
         .domain(colorThreshold.map(d => d.value))
         .range([0,1,2,3,4])
 
-
-        // calculate the threshold based on the value of the z axis
-        // map everything, but only add the threshold if the data point is contained within that binning
-        let values2 = cartesian(yAxisTicks, xAxisTicks).map(d => {
-            return {
-              x: d[0],
-              y: d[1],
-              value: this._mapDToV(d[0],d[1]),
-              threshold: thresholdScale(this._mapDToV(d[0],d[1]))
-            }
-          })
-        // console.log(values2)
-        let values = cartesian(yAxisTicks, xAxisTicks).map(d => {
-            return {
-              temp: d[0],
-              rh: d[1],
-              HI: HI(...d),
-              threshold: thresholdScale(HI(...d))
-            }
-          }) 
-        console.log(values2)
-        let formattedData = d3.nest()
-        .key(d => d.threshold)
-        .sortKeys(d3.ascending)
-        .key(d => d.rh)
-        .rollup(vals => d3.max(vals))
-        .entries(values)
-        .map(d => {
-            return {
-            key: d.key,
-            values: xAxisTicks.map(h => {
-                const v = d.values.find(f => +f.key === h);
-                return {
-                rh: h,
-                temp: v ? v.value.temp : 120
-                }
-            })
-            }
-        }).map( (d, i, arr) => {
-            d.values = d.values.map( (f, j) => {
-            f.prevTemp = i > 0 ? arr[i - 1].values[j].temp : 80;
-            return f;
-            })
-            return d;
+        let formattedData = values.map(d => {
+          return {
+            x: d.x,
+            y: d.y,
+            z: thresholdScale(d.z !== undefined ? d.z: 0)
+          }
         })
-
         return formattedData
     }
 
 
     _renderHeatTable () {
-        const { width, height, margin, thresholds, colors, ranges, xAxisLabel, yAxisLabel, colorThreshold } = this.state 
+        const { width, height, margin, thresholds, ranges, xAxisLabel, yAxisLabel, colorThreshold, bins, colors } = this.state 
         const { xMin, xMax, yMin, yMax } = ranges
-        const { data } = this.state
+        const data = this.pretendBackEndRequest(ranges, bins)
+        
 
         const xAxisTicks = d3.range(xMin,xMax,1)
         const yAxisTicks = d3.range(yMin,yMax,1).reverse()
 
 
-        
-        const chartData = this._formatData(xAxisTicks, yAxisTicks, data, colorThreshold)       
-        
+        const chartData = this._formatData(data, colorThreshold, bins)       
 
         const svg = d3.select('.heat-table').append('svg')
                             .attr('width', width)
@@ -115,97 +94,73 @@ export default class HeatTable extends React.Component {
           
         const chartWidth = width - margin.left - margin.right;
         const chartHeight = height - margin.top - margin.bottom;
+
+        const gridSizeX = Math.floor((chartWidth) / bins.xBins)
+        const gridSizeY = Math.floor((chartHeight) / bins.yBins)
         
         const chart = svg.append('g')
           .style('font-family', 'sans-serif')
           .attr('transform', `translate(${margin.left}, ${margin.top})`);
         
+        const cards = chart.selectAll('.card')
+          .data(chartData)
+          .enter()
+          .append('rect')
+          .attr('x', d => ( d.x - 1) * gridSizeX)
+          .attr('y', d => (d.y - 1) * gridSizeY)
+          .attr("rx", 4)
+          .attr("ry", 4)
+          .attr('width', gridSizeX)
+          .attr('height', gridSizeY)
+          .style('fill', colors[0])
+          .attr('transform', `translate(${gridSizeX}, ${gridSizeY})`)
+        
+        cards
+          .transition().duration(1000)
+          .style('fill', d => colors[d.z])
+
+        const tableLengthX = gridSizeX * bins.xBins
+        const tableLengthY = gridSizeY * bins.yBins
+
         const x = d3.scaleLinear()
           .domain(d3.extent(xAxisTicks))
-          .range([0, chartWidth]);
+          .range([0, tableLengthX])
         
         const y = d3.scaleLinear()
           .domain(d3.extent(yAxisTicks))
-          .range([chartHeight, 0]);
+          .range([tableLengthY, 0])
         
         chart.append("g")
-          .attr('transform', `translate(0, ${chartHeight + 2})`)
+          .attr('transform', `translate(0, ${tableLengthY})`)
           .style('font-size', '13px')
           .style('font-weight', 300)
           .call(d3.axisBottom(x).ticks(5))
-          .selectAll('.domain').remove();
+          .selectAll('.domain').remove()
         
         chart.append('text')
-          .attr('x', chartWidth)
-          .attr('y', chartHeight + 40)
+          .attr('x', Math.floor((tableLengthX / 5) * 3))
+          .attr('y', tableLengthY + 50) // replace 50 with value as function of fontsize
           .text(xAxisLabel)
           .style('text-anchor', 'end')
           .style('font-weight', 600)
-          .style('font-size', '14px');
+          .style('font-size', '14px')
         
         chart.append("g")
-          .attr('transform', 'translate(-2, 0)')
           .style('font-size', '13px')
           .style('font-weight', 300)
           .call(d3.axisLeft(y).ticks(5))
-          .selectAll('.domain').remove();
+          .selectAll('.domain').remove()
         
         chart.append('text')
-          .attr('x', 0)
-          .attr('y', -15)
+          .attr('x', Math.floor((tableLengthX / 5) * 3))
+          .attr('y', 50)
+          .attr('transform', 'rotate(90)')
           .text(yAxisLabel)
           .style('text-anchor', 'end')
           .style('font-weight', 600)
-          .style('font-size', '14px');
-          
-        const area = d3.area()
-          .x(d => x(d.rh))
-          .y0(d => y(d.temp))
-          .y1(d => y(d.prevTemp))
-          .curve(d3.curveStepAfter);
-          
-        chart.append('g')
-          .selectAll('path')
-          .data(chartData)
-          .enter().append("path")
-          .attr('d', d => area(d.values))
-          .style('fill', d => colors[+d.key])
-          .style('opacity', .8);
-          
-        chart.append('g')			
-          .attr('transform', `translate(0, ${chartHeight})`)
-          .call(d3.axisBottom(x)
-            .ticks(xAxisTicks.length)
-            .tickSize(-chartHeight)
-            .tickFormat('')
-          ).selectAll('line')
-          .style('stroke', 'white')
-          .style('stroke-width', 1)
-          .style('stroke-opacity', .5);
-          
-        chart.append('g')			
-          .call(d3.axisLeft(y)
-            .ticks(yAxisTicks.length)
-            .tickSize(-chartWidth)
-            .tickFormat('')
-          ).selectAll('line')
-          .style('stroke', 'white')
-          .style('stroke-width', 1)
-          .style('stroke-opacity', .5);
+          .style('font-size', '14px')
           
         chart.selectAll('.domain').remove();
-          
-        chart.append('g')
-          .selectAll('text')
-          .data(thresholds)
-          .enter().append('text')
-          .text(d => d.text)
-          .attr('transform', d => `translate(${x(d.H)}, ${y(d.T) - 2})`)
-          .attr('x', 0)
-          .attr('y', 0)
-          .style('text-anchor', 'middle')
-          .style('font-size', '13px')
-          .style('font-weight', 300);
           
         return svg.node();
 
